eca33e2fcf3226632e1e0c5098d87e33325d316d
Caleb Kim <dthturretbuddy@gmail.com>
2015-02-05 12:18:19 -0800

    Pretty much done

--- quizdroid/app/src/main/java/com/example/iguest/quizdroid/Answers.java
+++ quizdroid/app/src/main/java/com/example/iguest/quizdroid/Answers.java
@@ -1,9 +1,11 @@
 package com.example.iguest.quizdroid;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class Answers extends ActionBarActivity {
@@ -12,28 +14,91 @@ public class Answers extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answers);
+
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_answers, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
+/*Intent launch = getIntent();
+        String topic = launch.getStringExtra("topic");
+        int ans = launch.getIntExtra("ans", -1);
+        TextView bool_ans = (TextView) findViewById(R.id.bool_ans);
+        TextView user_ans = (TextView) findViewById(R.id.user_ans);
+        TextView correct_ans = (TextView) findViewById(R.id.correct_ans);
+        TextView total_num = (TextView) findViewById(R.id.total_num);
+        if (topic.equals("Math")){
+            if (ans == 1) {
+                bool_ans.setText("Correct!");
+                user_ans.setText("You put: " + "'If the starting numbers of coins are random, then the player who goes first will\n" +
+                        "most likely win,'");
+                correct_ans.setText("Correct Answer: " + "'If the starting numbers of coins are random, then the player who goes first will\n" +
+                        "most likely win,'");
+                total_num.setText("1/1 correct. \n" + "You're basically Archimedes");
+            } else if (ans == 2) {
+                bool_ans.setText("Wrong!");
+                user_ans.setText("You put: " + "'Winning positions are the\n" +
+                        "ones that have an even number of 1’s in each column, when written in base 2.'");
+                correct_ans.setText("Correct Answer: " + "'If the starting numbers of coins are random, then the player who goes first will\n" +
+                        "most likely win,'");
+                total_num.setText("0/1 correct. \n" + "These are losing positions not winning positions");
+            } else if (ans == 3){
+                bool_ans.setText("Wrong!");
+                user_ans.setText("You put: " + "'Regardless of how many starting number of coins there are, the player who goes first will win'");
+                correct_ans.setText("Correct Answer: " + "'If the starting numbers of coins are random, then the player who goes first will\n" +
+                        "most likely win,'");
+                total_num.setText("0/1 correct. \n" + "Nice try, but obviously wrong.");
+            } else {
+                bool_ans.setText("Wrong!");
+                user_ans.setText("You put: " + "'What the ...........'");
+                correct_ans.setText("Correct Answer: " + "'If the starting numbers of coins are random, then the player who goes first will\n" +
+                        "most likely win,'");
+                total_num.setText("0/1 correct. \n" + "You're not even trying");
+            }
+        } else if (topic.equals("Physics")){
+            if (ans == 1) {
+                bool_ans.setText("Wrong!");
+                user_ans.setText("You put: " + "'λ' = λ + h/mc(1 - cos θ)'");
+                correct_ans.setText("Correct Answer: " + "'Therefore, the photon bounces back with an essentially fixed E, independent of the initial E'");
+                total_num.setText("0/1 correct. \n" + "You should try again");
+            } else if (ans == 2) {
+                bool_ans.setText("Wrong!");
+                user_ans.setText("You put: " + "'If θ ≈ 0 (that is, not much scattering), then λ'!= λ, as expected.'");
+                correct_ans.setText("Correct Answer: " + "'Therefore, the photon bounces back with an essentially fixed E, independent of the initial E'");
+                total_num.setText("0/1 correct. \n" + "You're WRONG");
+            } else if (ans == 3){
+                bool_ans.setText("Correct!");
+                user_ans.setText("You put: " + "'Therefore, the photon bounces back with an essentially fixed E, independent of the initial E'");
+                correct_ans.setText("Correct Answer: " + "'Therefore, the photon bounces back with an essentially fixed E, independent of the initial E'");
+                total_num.setText("1/1 correct. \n" + "You're basically Newton");
+            } else {
+                bool_ans.setText("Wrong!");
+                user_ans.setText("You put: " + "'Seriously...?'");
+                correct_ans.setText("Correct Answer: " + "'Therefore, the photon bounces back with an essentially fixed E, independent of the initial E'");
+                total_num.setText("0/1 correct. \n" + "You're disappointing");
+            }
+        } else {
+            if (ans == 1) {
+                bool_ans.setText("Correct!");
+                user_ans.setText("You put: " + "'Ultimate Spiderman'");
+                correct_ans.setText("Correct Answer: " + "'Ultimate Spiderman'");
+                total_num.setText("1/1 correct. \n" + "You're basically Peter Parker");
+            } else if (ans == 2) {
+                bool_ans.setText("Correct!");
+                user_ans.setText("You put: " + "'Hawkeye'");
+                correct_ans.setText("Correct Answer: " + "'Hawkeye'");
+                total_num.setText("1/1 correct. \n" + "Bullseye");
+            } else if (ans == 3){
+                bool_ans.setText("Correct!");
+                user_ans.setText("You put: " + "'Inhumans'");
+                correct_ans.setText("Correct Answer: " + "'Inhumans'");
+                total_num.setText("1/1 correct. \n" + "You Mutant!");
+            } else {
+                bool_ans.setText("Correct!");
+                user_ans.setText("You put: " + "'all of the above'");
+                correct_ans.setText("Correct Answer: " + "'all of the above'");
+                total_num.setText("1/1 correct. \n" + "This is the true correct answer");
+            }
+        } */
--- quizdroid/app/src/main/java/com/example/iguest/quizdroid/Questions.java
+++ quizdroid/app/src/main/java/com/example/iguest/quizdroid/Questions.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 
 public class Questions extends ActionBarActivity {
     public String topic_field = "";
+    public int user_ans = 0;
 
 
     @Override
@@ -33,8 +35,7 @@ public class Questions extends ActionBarActivity {
                     "λ'= λ + (h/mc)(1-cos θ),\n" +
                     "where m is the mass of the electron. Note: The energy of a photon is E = hν = hc/λ.");
             q1.setText("λ' = λ + h/mc(1 - cos θ)");
-            q2.setText("If θ ≈ 0 (that is, not much scattering), then λ'" +
-                    " != λ, as expected.");
+            q2.setText("If θ ≈ 0 (that is, not much scattering), then λ'!= λ, as expected.");
             q3.setText("Therefore, the photon bounces back with an essentially fixed E, independent of the initial E");
             q4.setText("Seriously...?");
         } else if (topic_field.equals("Math")){
@@ -44,9 +45,9 @@ public class Questions extends ActionBarActivity {
                     "The goal is to be the person to remove the last coin(s).");
             q1.setText("If the starting numbers of coins are random, then the player who goes first will\n" +
                     "most likely win,");
-            q2.setText("Regardless of how many starting number of coins there are, the player who goes first will win");
-            q3.setText("Winning positions are the\n" +
+            q2.setText("Winning positions are the\n" +
                     "ones that have an even number of 1’s in each column, when written in base 2.");
+            q3.setText("Regardless of how many starting number of coins there are, the player who goes first will win");
             q4.setText("What the ...........");
         } else {
             top_question.setText("Which Marvel Comic run is the best?");
@@ -56,43 +57,64 @@ public class Questions extends ActionBarActivity {
             q4.setText("all of the above");
         }
 
-        /*Intent nextActivity = new Intent(Questions.this, Answers.class);
+        Button b = (Button) findViewById(R.id.button);
 
+        // On button click Open 2nd activity
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextActivity = new Intent(Questions.this, Answers.class);
 
-        if (nextActivity.resolveActivity(getPackageManager()) != null) {
-            startActivity(nextActivity); // opens a new activity
-        }
-        // code still runs asynchronously
+                // add data to be passed to next activity
+                nextActivity.putExtra("topic", topic_field);
+                nextActivity.putExtra("ans", user_ans);
 
-        finish(); // kill this instance self (this activity)*/
+                if (nextActivity.resolveActivity(getPackageManager()) != null) {
+                    startActivity(nextActivity);
+                }
+
+                finish(); // suicide
+            }
+        });
     }
 
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
-
+        //show button
+        Button finish = (Button) findViewById(R.id.button);
         // Check which radio button was clicked
         switch(view.getId()) {
             case R.id.q1:
                 if (checked) {
+                    setAnswer(1);
                     // Pirates are the best
                     break;
                 }
             case R.id.q2:
                 if (checked) {
+                    setAnswer(2);
                     // Ninjas rule
                     break;
                 }
             case R.id.q3:
                 if (checked) {
+                    setAnswer(3);
                     // blah is the blah
                     break;
                 }
             case R.id.q4:
                 if (checked) {
+                    setAnswer(4);
                     // blah is the blah
                     break;
                 }
         }
+        finish.setVisibility(View.VISIBLE);
     }
+
+    private void setAnswer(int i) {
+        user_ans = i;
+    }
+
 }
